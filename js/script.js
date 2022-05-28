4a7685d5f33129dade4544f7dd55bd3d56dd6dca
M9331L <matiasalas.e@gmail.com>
2022-05-27 22:12:25 -0300

    segunda carga

--- js/script.js
+++ js/script.js
@@ -26,38 +26,6 @@ function fixNav() {
 }
 
 
-//get the position y of the element in the page
-
-// let getPosition = (element) => {
-//     let y = 0;
-//     while (element) {
-//         y += element.offsetTop;
-//         element = element.offsetParent;
-//     }
-//     return y;
-// }
-
-// let change_color_bar = getPosition(document.querySelector('#clientes'))
-
-
-// let hamburguer_color_bar = document.querySelector('.hamburguer');
-// let navMenu_color_bar = document.querySelector('.nav-menu');
-
-// let nav2 = document.querySelector('.navbar')
-// window.addEventListener('scroll', fixNav2)
-// function fixNav2() {
-//     if (window.scrollY > nav.offsetHeight + change_color_bar - 150) {
-//         nav2.classList.add('active2')
-//         hamburguer_color_bar.classList.add('active_white')
-//         navMenu_color_bar.classList.add('active_white')
-//     } else {
-//         nav2.classList.remove('active2')
-//         hamburguer_color_bar.classList.remove('active_white')
-//         navMenu_color_bar.classList.remove('active_white')
-//     }
-// }
-
-
 
 const nav_menu = document.querySelector('.nav-menu')
 window.addEventListener('scroll', fixMenu)
@@ -94,61 +62,4 @@ function clickHandler(e) {
         behavior: "smooth"
     });
 }
-
-
-// carousel
-
-
-// let images = [...document.querySelectorAll('.carrousel-img')];
-// images.forEach((image, idx) => {
-//     image.style.backgroundImage = `url(./js/logo/logo_${idx + 1}.jpg)`;
-// });
-
-
-
-// const carrusel = document.querySelector(".carrusel-items");
-
-
-
-// let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
-
-
-// let intervalo = null;
-// let step = 1;
-
-
-// const start = () => {
-
-//     intervalo = setInterval(function () {
-//         carrusel.scrollLeft = carrusel.scrollLeft + step;
-
-//         if (carrusel.scrollLeft === maxScrollLeft) {
-
-//             step = step * -1;
-//         } else if (carrusel.scrollLeft === 0) {
-
-//             step = step * -1;
-
-//         }
-//     }, 10);
-// };
-
-
-
-
-
-// const stop = () => {
-//     clearInterval(intervalo);
-// };
-
-// carrusel.addEventListener("mouseover", () => {
-//     stop();
-// });
-
-// carrusel.addEventListener("mouseout", () => {
-//     start();
-// });
-
-// start();
-
 
